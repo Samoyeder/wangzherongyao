1876968f6d25eb5e20af290c2b929f25ea9fdf68
Samoyeder <1565828704@qq.com>
2020-10-02 23:20:14 +0800

    新增英雄编辑

--- admin/src/views/HeroEdit.vue
+++ admin/src/views/HeroEdit.vue
@@ -5,6 +5,9 @@
       <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
       </el-form-item>
+      <el-form-item label="称号">
+        <el-input v-model="model.title"></el-input>
+      </el-form-item>
       <el-form-item label="头像">
         <el-upload
           class="avatar-uploader"
@@ -16,39 +19,90 @@
           <i v-else class="el-icon-plus avatar-uploader-icon"></i>
         </el-upload>
       </el-form-item>
+      <el-form-item label="类型">
+        <el-select v-model="model.categories" multiple>
+          <el-option
+            v-for="item in categories"
+            :label="item.name"
+            :value="item._id"
+            :key="item._id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="难度">
+        <el-rate
+          style="margin-top: 0.7rem"
+          :max="9"
+          show-score
+          v-model="model.scores.difficult"
+        ></el-rate>
+      </el-form-item>
+      <el-form-item label="技能">
+        <el-rate
+          style="margin-top: 0.7rem"
+          :max="9"
+          show-score
+          v-model="model.scores.skills"
+        ></el-rate>
+      </el-form-item>
+      <el-form-item label="攻击">
+        <el-rate
+          style="margin-top: 0.7rem"
+          :max="9"
+          show-score
+          v-model="model.scores.attack"
+        ></el-rate>
+      </el-form-item>
+      <el-form-item label="生存">
+        <el-rate
+          style="margin-top: 0.7rem"
+          :max="9"
+          show-score
+          v-model="model.scores.survive"
+        ></el-rate>
+      </el-form-item>
+
+      <el-form-item label="顺风出装">
+        <el-select v-model="model.items1" multiple>
+          <el-option
+            v-for="item in items"
+            :label="item.name"
+            :value="item._id"
+            :key="item._id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="逆风出装">
+        <el-select v-model="model.items2" multiple>
+          <el-option
+            v-for="item in items"
+            :label="item.name"
+            :value="item._id"
+            :key="item._id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="使用技巧">
+        <el-input type="textarea" v-model="model.usageTips"></el-input>
+      </el-form-item>
+
+      <el-form-item label="对抗技巧">
+        <el-input type="textarea" v-model="model.battleTips"></el-input>
+      </el-form-item>
+
+      <el-form-item label="团战思路">
+        <el-input type="textarea" v-model="model.teamTips"></el-input>
+      </el-form-item>
+
       <el-button type="primary" native-type="submit">保存</el-button>
     </el-form>
   </div>
 </template>
 
-<style>
-  .avatar-uploader .el-upload {
-    border: 1px dashed #d9d9d9;
-    border-radius: 6px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-  }
-  .avatar-uploader .el-upload:hover {
-    border-color: #409EFF;
-  }
-  .avatar-uploader-icon {
-    font-size: 28px;
-    color: #8c939d;
-    width: 178px;
-    height: 178px;
-    line-height: 178px;
-    text-align: center;
-  }
-  .avatar {
-    width: 178px;
-    height: 178px;
-    display: block;
-  }
-</style>
 
 <script>
-
 export default {
   name: "ItemEdit",
   props: {
@@ -56,18 +110,25 @@ export default {
   },
   data() {
     return {
+      categories: [],
+      items: [],
       model: {
-        avatar: '',
-        name: ''
+        avatar: "",
+        name: "",
+        scores: {
+          difficult: 0,
+        },
+        usageTips: "",
+        battleTips: "",
+        teamTips: "",
       },
     };
   },
   methods: {
-    afterUpload(res){
-
+    afterUpload(res) {
       // 理论上用this.model.icon = res.url也是可以的，但是因为vue响应式的原因，之前model并没有icon，所以可能会造成赋值不上去，这个时候用$set，显式的告诉vue。
       // this.$set(this.model, 'avatar', res.url)
-      this.model.avatar = res.url
+      this.model.avatar = res.url;
     },
     // 将新建的分类提交
     async save() {
@@ -88,14 +149,51 @@ export default {
     // 获取到需要编辑的分类的名称，将其传给model从而在input中显示出来
     async fetch() {
       const res = await this.$http.get(`rest/heros/${this.id}`);
-      this.model = res.data;
+      // this.model = res.data;
+      this.model = Object.assign({}, this.model, res.data);
+    },
+    // 获取到都有哪些分类（职业）
+    async fetchCategories() {
+      const res = await this.$http.get(`rest/categories`);
+      this.categories = res.data;
+    },
+    // 获取到都有哪些物品
+    async fetchItems() {
+      const res = await this.$http.get(`rest/items`);
+      this.items = res.data;
     },
   },
   created() {
+    this.fetchCategories();
+    this.fetchItems();
     this.id && this.fetch();
   },
 };
 </script>
 
-<style scoped>
+
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
 </style>
--- server/models/Hero.js
+++ server/models/Hero.js
@@ -2,12 +2,93 @@ const mongoose = require('mongoose')
 
 // 创建集合规则
 const schema = new mongoose.Schema({
+  // 英雄名称
   name: {
     type: String
   },
+  // 英雄头像
   avatar: {
     type: String
-  }
+  },
+  // 英雄称号（逐日之弓——后裔）
+  title: {
+    type: String
+  },
+  // 英雄职业（射手、法师等）
+  categories: [{
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'Category'
+  }],
+  // 英雄评分
+  scores: {
+    // 英雄上手难度
+    difficult: {
+      type: Number
+    },
+    // 技能得分
+    skills: {
+      type: Number
+    },
+    // 攻击得分
+    attack: {
+      type: Number
+    },
+    // 生存得分
+    survive: {
+      type: Number
+    },
+  },
+  // 英雄技能
+  skills: [{
+    // 技能图标
+    icon: {
+      type: String
+    },
+    // 技能名称
+    name: {
+      type: String
+    },
+    // 技能详细描述
+    description: {
+      type: String
+    },
+    // 小贴士
+    tips: {
+      type: String
+    }
+  }],
+  // 顺风出装推荐
+  items1: [{
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'Item'
+  }],
+  // 逆风出装推荐
+  items2: [{
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'Item'
+  }],
+  // 使用技巧
+  usageTips: {
+    type: String
+  },
+  // 对抗技巧
+  battleTips: {
+    type: String
+  },
+  // 团战技巧
+  teamTips: {
+    type: String
+  },
+  // 英雄最佳搭档
+  partners: [{
+    hero: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'Hero'
+    },
+    description: {
+      type: String
+    }
+  }]
 })
 
 // 使用集合规则创建集合，创建的集合叫categories
